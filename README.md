308575fcd616aed48e9748a750079a45dba7515a
l <l@l.hsd1.nj.comcast.net>
2023-03-28 04:12:59 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# React Personal Website
+
+ilovecodingallday.com
+
+
---
+++ src/pages/Info.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+
+
+
+
+
+
+
+
+
+
+
+function Info() {
+    return (
+        <div className="info">
+
+
+           <p>
+               Hello and welcome to my website! I'm Michael McNulty.
+           </p>
+
+
+
+
+        </div>
+    );
+}
+
+export default Info;
